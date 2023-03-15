82c825c48763f96ad7721b9f28d561f12128f114
Dameng <313880747@qq.com>
2023-03-15 14:31:09 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
-
+///
+/// # hello
 fn main() {
     let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
+    
+    println!("Hello, {name}!");
 }
